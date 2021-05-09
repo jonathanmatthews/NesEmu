d42e3494327f6bd2ac0b7ef14c03b80c41317ba5
Jonathan <44363860+jonathanmatthews@users.noreply.github.com>
2021-05-09 15:34:39 +0100

    Add registers

--- src/Emulation/Registers.cs
+++ src/Emulation/Registers.cs
@@ -2,7 +2,19 @@
 
 namespace NesEmu.Emulation
 {
-    public struct Registers
+    public class Registers
     {
+        public ushort ProgramCounter;
+        public byte StackPointer;
+        public byte Accumulator;
+        public byte IndexX;
+        public byte IndexY;
+        public bool Carry;
+        public bool Zero;
+        public bool InterruptDisable;
+        public bool DecimalMode;
+        public bool Break;
+        public bool Overflow;
+        public bool Negative;
     }
 }
