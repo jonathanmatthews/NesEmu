a09b4e760c2ec61e1c85f25d1085225c64e451ad
Jonathan Matthews <44363860+jonathanmatthews@users.noreply.github.com>
2021-05-16 19:59:17 +0100

    Add official instructions (without decode) 0x00-0x2E

--- src/Emulation/InstructionCollection.cs
+++ src/Emulation/InstructionCollection.cs
@@ -7,10 +7,212 @@ namespace NesEmu.Emulation
         private readonly Registers _registers;
         private readonly NesMemory _memory;
 
-        public InstructionCollection(Registers registers, NesMemory memory)
+        public InstructionCollection(
+            Registers registers,
+            NesMemory memory)
         {
             _registers = registers;
             _memory = memory;
+        }
+
+        /// <summary>
+        /// Break
+        /// </summary>
+        public void BRK()
+        {
+            throw new NotImplementedException("BRK not implemented.");
+        }
+
+        /// <summary>
+        /// OR data at memory address with accumulator and store in accumulator
+        /// </summary>
+        public void ORA(ushort operandAddress)
+        {
+            _registers.Accumulator |= _memory[operandAddress];
+            _registers.Zero = _registers.Accumulator == 0;
+            _registers.Negative = _registers.Accumulator > 127;
+        }
+
+        /// <summary>
+        /// OR data given explicitly with accumulator and store in accumulator
+        /// </summary>
+        public void ORA(byte data)
+        {
+            _registers.Accumulator |= data;
+            _registers.Zero = _registers.Accumulator == 0;
+            _registers.Negative = _registers.Accumulator > 127;
+        }
+
+        /// <summary>
+        /// Bit shift left the memory at address by 1
+        /// </summary>
+        public void ASL(ushort operandAddress)
+        {
+            var input = _memory[operandAddress];
+            var result = (byte)(input << 1);
+
+            _registers.Carry = (input & 0b10000000) > 0;
+            _registers.Negative = (result & 0b10000000) > 0;
+            _registers.Zero = result == 0;
+            _memory[operandAddress] = result;
+        }
+
+        /// <summary>
+        /// Bit shift left the accumulator by 1
+        /// </summary>
+        public void ASL()
+        {
+            _registers.Carry = (_registers.Accumulator & 0b10000000) > 0;
+            _registers.Accumulator <<= 1;
+            _registers.Negative = (_registers.Accumulator & 0b10000000) > 0;
+            _registers.Zero = _registers.Accumulator == 0;
+        }
+
+        /// <summary>
+        /// Push processor status onto stack
+        /// </summary>
+        public void PHP()
+        {
+            _memory.PushStatusToStack(false);
+        }
+
+        /// <summary>
+        /// Pull processor status from stack
+        /// </summary>
+        public void PLP()
+        {
+            _memory.PopStatusFromStack();
+        }
+
+        /// <summary>
+        /// Branch on plus, returns new PC value
+        /// </summary>
+        public ushort BPL(byte relativeAddress)
+        {
+            if (!_registers.Negative)
+            {
+                var PC = (ushort)(_registers.ProgramCounter + relativeAddress);
+                if (relativeAddress > 127) PC -= 256;
+                return PC;
+            }
+            else
+                return (ushort)(_registers.ProgramCounter + 2);
+        }
+
+        /// <summary>
+        /// Branch on minus, returns new PC value
+        /// </summary>
+        public ushort BMI(byte relativeAddress)
+        {
+            if (_registers.Negative)
+            {
+                var PC = (ushort)(_registers.ProgramCounter + relativeAddress);
+                if (relativeAddress > 127) PC -= 256;
+                return PC;
+            }
+            else
+                return (ushort)(_registers.ProgramCounter + 2);
+        }
+
+        /// <summary>
+        /// Clear carry flag
+        /// </summary>
+        public void CLC()
+        {
+            _registers.Carry = false;
+        }
+
+        /// <summary>
+        /// Jump to subroutine, returns new PC value
+        /// </summary>
+        public ushort JSR(ushort subroutineAddress)
+        {
+            // Technically, this is the address right before the next instruction,
+            // but RTS will increment by 1 to correct for this.
+            var nextInstruction = (ushort)(_registers.ProgramCounter + 2);
+            var significantByte = (byte)(nextInstruction >> 4);
+            var insignificantByte = (byte)nextInstruction; // Modulo 0x100
+            
+            // Push order must match RTS
+            _memory.PushStack(significantByte);
+            _memory.PushStack(insignificantByte);
+
+            return subroutineAddress;
+        }
+
+        /// <summary>
+        /// Return from subroutine, returns new PC value
+        /// </summary>
+        public ushort RTS()
+        {
+            // Pop order must match JSR
+            var insignificantByte = _memory.PopStack();
+            var significantByte = _memory.PopStack() << 4;
+            return (ushort)(insignificantByte + significantByte + 1);
+        }
+
+        /// <summary>
+        /// AND of accumulator and memory at address, stores result in accumulator
+        /// </summary>
+        public void AND(ushort operandAddress)
+        {
+            _registers.Accumulator &= _memory[operandAddress];
+            _registers.Zero = _registers.Accumulator == 0;
+            _registers.Negative = _registers.Accumulator > 127;
+        }
+
+        /// <summary>
+        /// AND data given explicitly with accumulator, store result in accumulator
+        /// </summary>
+        public void AND(byte data)
+        {
+            _registers.Accumulator &= data;
+            _registers.Zero = _registers.Accumulator == 0;
+            _registers.Negative = _registers.Accumulator > 127;
+        }
+
+        /// <summary>
+        /// BIT test data at address without modifying
+        /// </summary>
+        public void BIT(ushort operandAddress)
+        {
+            var data = _memory[operandAddress];
+            var result = _registers.Accumulator & data;
+
+            _registers.Zero = result == 0;
+            _registers.Negative = data > 127;
+            _registers.Overflow = (data & 0b01000000) > 0;
+        }
+
+        /// <summary>
+        /// Rotate left the memory at address with carry flag
+        /// </summary>
+        public void ROL(ushort operandAddress)
+        {
+            var data = _memory[operandAddress];
+            var result = (byte)(data << 1);
+            if (_registers.Carry) result++;
+
+            _registers.Carry = data > 127;
+            _registers.Zero = result == 0;
+            _registers.Negative = result > 127;
+
+            _memory[operandAddress] = (byte)result;
+        }
+
+        /// <summary>
+        /// Rotate left the value in the accumulator with carry flag
+        /// </summary>
+        public void ROL()
+        {
+            var result = (byte)(_registers.Accumulator << 1);
+            if (_registers.Carry) result++;
+
+            _registers.Carry = _registers.Accumulator > 127;
+            _registers.Zero = result == 0;
+            _registers.Negative = result > 127;
+
+            _registers.Accumulator = (byte)result;
         }
     }
 }
--- src/Emulation/NesMemory.cs
+++ src/Emulation/NesMemory.cs
@@ -22,7 +22,8 @@ namespace NesEmu.Emulation
                 if (pointer < 0x2000) return _internalMemory[pointer % 0x800];
                 else if (pointer < 0x4000) return _ppuRegisters[pointer % 0x8];
                 else if (pointer < 0x4018) return _apuIoRegisters[pointer - 0x4000];
-                else if (pointer < 0x4020) throw new InvalidOperationException("CPU test mode disabled");
+                else if (pointer < 0x4020)
+                    throw new InvalidOperationException("CPU test mode disabled");
                 else return _cartridgeSpace[pointer - 0xbfe0];
             }
 
@@ -31,7 +32,8 @@ namespace NesEmu.Emulation
                 if (pointer < 0x2000) _internalMemory[pointer % 0x800] = value;
                 else if (pointer < 0x4000) _ppuRegisters[pointer % 0x8] = value;
                 else if (pointer < 0x4018) _apuIoRegisters[pointer - 0x4000] = value;
-                else if (pointer < 0x4020) throw new InvalidOperationException("CPU test mode disabled");
+                else if (pointer < 0x4020)
+                    throw new InvalidOperationException("CPU test mode disabled");
                 else _cartridgeSpace[pointer - 0xbfe0] = value;
             }
         }
@@ -41,5 +43,33 @@ namespace NesEmu.Emulation
 
         public byte PushStack(byte data)
             => _internalMemory[_registers.StackPointer-- + 0x100] = data;
+
+        public void PushStatusToStack(bool isInterrupt)
+        {
+            byte status = 0;
+            if (_registers.Negative) status += 0b10000000;
+            if (_registers.Overflow) status += 0b01000000;
+            status += 0b00100000; // Always set to 1
+            if (!isInterrupt) status += 0b00010000; // Set by instructions, not interrupts
+            if (_registers.DecimalMode) status += 0b00001000;
+            if (_registers.InterruptDisable) status += 0b00000100;
+            if (_registers.Zero) status += 0b00000010;
+            if (_registers.Carry) status += 0b00000001;
+
+            PushStack(status);
+        }
+
+        public void PopStatusFromStack()
+        {
+            byte status = PopStack();
+
+            _registers.Negative = (status & 0b10000000) > 0;
+            _registers.Overflow = (status & 0b01000000) > 0;
+            // Don't load B flag in bits 5 and 4
+            _registers.DecimalMode = (status & 0b00001000) > 0;
+            _registers.InterruptDisable = (status & 0b00000100) > 0;
+            _registers.Zero = (status & 0b00000010) > 0;
+            _registers.Carry = (status & 0b00000001) > 0;
+        }
     }
 }
