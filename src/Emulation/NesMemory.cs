5e40dcc26e877c69486666ed494fe4d491adf572
Jonathan Matthews <44363860+jonathanmatthews@users.noreply.github.com>
2021-05-16 13:48:09 +0100

    Add memory mirroring and mapping

--- src/Emulation/NesMemory.cs
+++ src/Emulation/NesMemory.cs
@@ -4,18 +4,42 @@ namespace NesEmu.Emulation
 {
     public class NesMemory
     {
-        private readonly byte[] _memory = new byte[0x10000];
+        private readonly byte[] _internalMemory = new byte[0x800];
+        private readonly byte[] _ppuRegisters = new byte[0x8];
+        private readonly byte[] _apuIoRegisters = new byte[0x18];
+        private readonly byte[] _cartridgeSpace = new byte[0xbfe0];
+        private readonly Registers _registers;
+
+        public NesMemory(Registers registers)
+        {
+            _registers = registers;
+        }
 
         public byte this[ushort pointer]
         {
-            get => _memory[pointer];
-            set => _memory[pointer] = value;
+            get
+            {
+                if (pointer < 0x2000) return _internalMemory[pointer % 0x800];
+                else if (pointer < 0x4000) return _ppuRegisters[pointer % 0x8];
+                else if (pointer < 0x4018) return _apuIoRegisters[pointer - 0x4000];
+                else if (pointer < 0x4020) throw new InvalidOperationException("CPU test mode disabled");
+                else return _cartridgeSpace[pointer - 0xbfe0];
+            }
+
+            set
+            {
+                if (pointer < 0x2000) _internalMemory[pointer % 0x800] = value;
+                else if (pointer < 0x4000) _ppuRegisters[pointer % 0x8] = value;
+                else if (pointer < 0x4018) _apuIoRegisters[pointer - 0x4000] = value;
+                else if (pointer < 0x4020) throw new InvalidOperationException("CPU test mode disabled");
+                else _cartridgeSpace[pointer - 0xbfe0] = value;
+            }
         }
 
-        public byte GetStackValue(byte stackAddress)
-            => _memory[0x0100 + stackAddress];
+        public byte PopStack()
+            => _internalMemory[++_registers.StackPointer + 0x100];
 
-        public void SetStackValue(byte stackAddress, byte value)
-            => _memory[0x0100 + stackAddress] = value;
+        public byte PushStack(byte data)
+            => _internalMemory[_registers.StackPointer-- + 0x100] = data;
     }
 }
--- src/Emulation/Registers.cs
+++ src/Emulation/Registers.cs
@@ -5,14 +5,14 @@ namespace NesEmu.Emulation
     public class Registers
     {
         public ushort ProgramCounter;
-        public byte StackPointer;
+        public byte StackPointer = 0xff;
         public byte Accumulator;
         public byte IndexX;
         public byte IndexY;
         public bool Carry;
         public bool Zero;
         public bool InterruptDisable;
-        public bool DecimalMode;
+        public bool DecimalMode; // Unused in NES
         public bool Break;
         public bool Overflow;
         public bool Negative;
